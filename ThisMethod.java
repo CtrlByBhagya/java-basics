7f0d5067690f0b6efca1c8ee746ff4c6535be51d
CtrlByBhagya <mishu.08nov23@gmail.com>
2026-05-24 21:00:05 +0530

    Added ThisMethod Code

---
+++ ThisMethod.java
@@ -0,0 +1,28 @@
+class Human{
+    private int age;
+    private String name;
+
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+}
+
+public class ThisMethod {
+    public static void main(String a[]){
+        Human obj=new Human();
+        obj.setAge(20);
+        obj.setName("bhagyashree");
+        System.out.println(obj.getName()+":"+obj.getAge());
+    }
+
+    
+}
